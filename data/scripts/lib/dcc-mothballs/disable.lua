0165fcf36b68f6b003d642727628cd25278dcc17
darkconsole <darkcee.legit@gmail.com>
2017-03-28 21:34:04 -0500

    client side enable/disable msg seems solid now

--- data/scripts/lib/dcc-mothballs/disable.lua
+++ data/scripts/lib/dcc-mothballs/disable.lua
@@ -5,50 +5,42 @@ darkconsole <darkcee.legit@gmail.com>
 This script disables the script that puts a ship in mothballs.
 ----------------------------------------------------------------------------]]--
 
-function ClientTerminate(Title,Text)
+function initialize()
+
 	if(onServer())
 	then
-		invokeClientFunction(Player(),"ClientTerminate",Title,Text)
+		local Ship = Entity(Player().craftIndex)
+		local Script = "lib/dcc-mothballs/mothballed.lua"
+
+		if(Ship == nil or Ship.type ~= EntityType.Ship)
+		then
+			print("[Mothballs] You are not currently controlling a ship.")
+			return terminate()
+		end
+
+		Ship:removeScript(Script)
+		print("[Mothballs] " .. Ship.name .. " is no longer mothballed.")
+
+		-- ping the client to show a message about it.
+		deferredCallback(
+			1, "WeDoneHereAndThere",
+			"Back From Retirement",
+			"The " .. Ship.name .. " now needs a full crew."
+		)
+
+		return
+	end
+end
+
+function WeDoneHereAndThere(Title,Text)
+	if(onServer())
+	then
+		invokeClientFunction(Player(),"WeDoneHereAndThere",Title,Text)
+		terminate()
 		return
 	end
 
 	displayMissionAccomplishedText(Title,Text)
 	terminate()
 	return
-end
-
-if(onServer())
-then
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-function initialize()
-
-	local Ship = Entity(Player().craftIndex)
-
-	if(Ship == nil or Ship.type ~= EntityType.Ship)
-	then
-		print("[Mothballs] You are not currently controlling a ship.")
-		return terminate()
-	end
-
-	Ship:removeScript("lib/dcc-mothballs/mothballed.lua")
-	print("[Mothballs] " .. Ship.name .. " is no longer mothballed.")
-
-	-- ping the client to show a message about it.
-	deferredCallback(
-		1, "ClientTerminate",
-		"Back From Retirement",
-		"The " .. Ship.name .. " now needs a full crew."
-	)
-
-	return terminate()
-end
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 end
--- data/scripts/lib/dcc-mothballs/enable.lua
+++ data/scripts/lib/dcc-mothballs/enable.lua
@@ -5,56 +5,47 @@ darkconsole <darkcee.legit@gmail.com>
 This script enables the script that puts a ship in mothballs.
 ----------------------------------------------------------------------------]]--
 
-function ClientTerminate(Title,Text)
+function initialize()
+
 	if(onServer())
 	then
-		invokeClientFunction(Player(),"ClientTerminate",Title,Text)
+		local Ship = Entity(Player().craftIndex)
+		local Script = "lib/dcc-mothballs/mothballed.lua"
+
+		if(Ship == nil or Ship.type ~= EntityType.Ship)
+		then
+			print("[Mothballs] You are not currently controlling a ship.")
+			return terminate()
+		end
+
+		if(not Ship:hasScript(Script))
+		then
+			Ship:addScript(Script)
+			print("[Mothballs] " .. Ship.name .. " is now mothballed.")
+		else
+			print("[Mothballs] " .. Ship.name .. " is already mothballed.")
+		end
+
+		-- ping the client to show a message about it.
+		deferredCallback(
+			1, "WeDoneHereAndThere",
+			"Mothballed",
+			"The " .. Ship.name .. " is ready for a skeleton crew."
+		)
+
+		return
+	end
+end
+
+function WeDoneHereAndThere(Title,Text)
+	if(onServer())
+	then
+		invokeClientFunction(Player(),"WeDoneHereAndThere",Title,Text)
+		terminate()
 		return
 	end
 
 	displayMissionAccomplishedText(Title,Text)
 	terminate()
 	return
-end
-
-if(onServer())
-then
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-function initialize()
-
-	local Ship = Entity(Player().craftIndex)
-	local Script = "lib/dcc-mothballs/mothballed.lua"
-
-	if(Ship == nil or Ship.type ~= EntityType.Ship)
-	then
-		print("[Mothballs] You are not currently controlling a ship.")
-		return terminate()
-	end
-
-	if(not Ship:hasScript(Script))
-	then
-		Ship:addScript(Script)
-		print("[Mothballs] " .. Ship.name .. " is now mothballed.")
-	else
-		print("[Mothballs] " .. Ship.name .. " is already mothballed.")
-	end
-
-	-- ping the client to show a message about it.
-	deferredCallback(
-		1, "ClientTerminate",
-		"Mothballed",
-		"The " .. Ship.name .. " is ready for a skeleton crew."
-	)
-
-	return terminate()
-end
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 end
